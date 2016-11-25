9f2e9c8bdae0b3e5dfad12c16880b1f0703c2dc5
Mohamed Ezzaouia <ezzaouia.mohamed@gmail.com>
2016-11-26 00:15:05 +0100

    adding express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 module.exports = require('./lib');
+
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist/"));
+app.use(gzippo.staticGzip("" + __dirname + "/src/"));
+app.listen(process.env.PORT || 5000);
