fdceb6ba4d34bafee1986ad58cabbcdbdc3b8c54
Mohamed Ezzaouia <ezzaouia.mohamed@gmail.com>
2016-11-26 00:17:02 +0100

    updating static folder

--- app.js
+++ app.js
@@ -8,6 +8,5 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist/"));
-app.use(gzippo.staticGzip("" + __dirname + "/src/"));
+app.use(gzippo.staticGzip("" + __dirname ));
 app.listen(process.env.PORT || 5000);
